05c53ffc57c106d7c9971898ef76151c8a7770b9
zhaosongtao163 <zhaosongtao163@163.com>
2018-11-12 15:23:58 +0800

    add

--- src/main/java/com/stzhao/bootdemo/Hello.java
+++ src/main/java/com/stzhao/bootdemo/Hello.java
@@ -1,6 +1,8 @@
 package com.stzhao.bootdemo;
 
 import com.stzhao.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,9 @@ import java.util.List;
 
 @RestController
 public class Hello {
+
+    private static Logger logger = LoggerFactory.getLogger(Hello.class);
+
 
     @Autowired
     TestService testService;
@@ -19,7 +24,7 @@ public class Hello {
 
     List list = testService.queryAll();
 
-
+        logger.info("hehehehe");
         return "laoZ2";
 
 
