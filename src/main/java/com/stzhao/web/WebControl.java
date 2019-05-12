41a8ee29f428c19c1361213405dc2ac338ca286b
laoZ <zhaosongtao163@163.com>
2019-05-12 18:25:41 +0800

    add log

--- src/main/java/com/stzhao/web/WebControl.java
+++ src/main/java/com/stzhao/web/WebControl.java
@@ -1,14 +1,21 @@
 package com.stzhao.web;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stzhao.bootdemo.Hello;
+
 @RestController
 public class WebControl {
-
+	
+	private static Logger log = LoggerFactory.getLogger(WebControl.class);
+	
     @GetMapping("/hello")
     public String hello() {
+    	log.info("start");
         return "laoZ1234";
     }
 }
