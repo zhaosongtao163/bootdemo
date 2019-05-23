7a612317221f46ba80a445d920c8ee35b30afda9
laoZ <zhaosongtao163@163.com>
2019-05-23 22:39:48 +0800

    9999

--- src/test/java/com/stzhao/bootdemo/BootdemoApplicationTests.java
+++ src/test/java/com/stzhao/bootdemo/BootdemoApplicationTests.java
@@ -15,9 +15,9 @@ public class BootdemoApplicationTests {
 		
 // 		
 		
-		System.out.println("az666777");
+		System.out.println("az666777888");
 // 		
-		System.out.println(3333);
+		System.out.println(33333333);
 		
 //		aaa
 		
